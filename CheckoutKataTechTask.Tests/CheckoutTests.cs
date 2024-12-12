260f295445c4792704d149e55553170cba40a821
Usman Hussain <Usmanh770@gmail.com>
2024-12-12 11:12:19 +0000

    Added test for scanning multiple items

--- CheckoutKataTechTask.Tests/CheckoutTests.cs
+++ CheckoutKataTechTask.Tests/CheckoutTests.cs
@@ -20,4 +20,29 @@
         // Assert
         Assert.Equal(50, total);
     }
+
+
+    [Fact]
+    public async Task WhenScanningMultipleItems_ShouldReturnCorrectTotal()
+    {
+        // Arrange
+        var productAId = Guid.NewGuid();
+        var productBId = Guid.NewGuid();
+        var repository = new InMemoryProductRepository();
+
+        await repository.AddAsync(new Product(productAId, "A", 50, "Product A"));
+        await repository.AddAsync(new Product(productBId, "B", 30, "Product B"));
+
+        var checkout = new Checkout(repository,
+            new DiscountService(repository) 
+           );
+
+        // Act
+        await checkout.ScanAsync(productAId);
+        await checkout.ScanAsync(productBId);
+        var total = await checkout.GetTotalPriceAsync();
+
+        // Assert
+        Assert.Equal(80, total);
+    }
 }
