58df7df76c2d57108489b3b01132b36a6bb236db
Usman Hussain <Usmanh770@gmail.com>
2024-12-12 12:48:44 +0000

    Added a console main function to display a discount rule how it would be applied

--- CheckoutKataTechTask.Core/Program.cs
+++ CheckoutKataTechTask.Core/Program.cs
@@ -1,9 +1,34 @@
-﻿using static Program;
-
+﻿
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var productRepository = new InMemoryProductRepository();
+        var discountService = new DiscountService(productRepository);
+
+        var checkout = new Checkout(productRepository, discountService);
+
+        // Add products
+        var productId = Guid.NewGuid();
+        await productRepository.AddAsync(new Product(productId, "A", 50, "Product A"));
+
+        // Add discount rules
+        discountService.AddDiscountRule(
+            new DiscountRule(productId, new BulkDiscountStrategy(3, 130)));
+
+        // Use checkout
+        try
+        {
+            List<Guid> scannedItems = new List<Guid>() { productId, productId, productId };
+
+            await checkout.ScanMultipleAsync(scannedItems);
+            var total = await checkout.GetTotalPriceAsync();
+            Console.WriteLine($"Total price: {total}");
+        }
+        catch (Exception ex)
+        {
+          //  logger.LogInformation("An error occurred during checkout");
+        }
     }
 }
+
