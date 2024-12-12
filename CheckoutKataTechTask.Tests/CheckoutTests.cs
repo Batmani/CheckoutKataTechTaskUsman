574b1cb0c001c53923d0298bc014585be73df265
Usman Hussain <Usmanh770@gmail.com>
2024-12-12 11:07:39 +0000

    Added first test for scanning single item with assertion comparison

---
+++ CheckoutKataTechTask.Tests/CheckoutTests.cs
@@ -0,0 +1,23 @@
+﻿public class CheckoutTests
+{
+    [Fact]
+    public async Task WhenScanningOneItem_ShouldReturnCorrectPrice()
+    {
+        // Arrange
+        var productId = Guid.NewGuid();
+        var product = new Product(productId, "A", 50, "Product A");
+        var repository = new InMemoryProductRepository();
+        await repository.AddAsync(product);
+
+        var checkout = new Checkout(repository,
+            new DiscountService(repository),
+            NullLogger<Checkout>.Instance);
+
+        // Act
+        await checkout.ScanAsync(productId);
+        var total = await checkout.GetTotalPriceAsync();
+
+        // Assert
+        Assert.Equal(50, total);
+    }
+}
