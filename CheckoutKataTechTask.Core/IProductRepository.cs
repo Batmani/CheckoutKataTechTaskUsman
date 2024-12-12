06263297fe60ec1f34bca219cb0a6148c5aa4bce
Usman Hussain <Usmanh770@gmail.com>
2024-12-12 12:28:12 +0000

    Refactored repository pattern to include CRUD operations

---
+++ CheckoutKataTechTask.Core/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid id);
+    Task<Product?> GetByCodeAsync(string code);
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+}
---
+++ CheckoutKataTechTask.Core/InMemoryProductRepository.cs
@@ -0,0 +1,33 @@
+﻿public class InMemoryProductRepository : IProductRepository
+{
+    private readonly Dictionary<Guid, Product> _products = new();
+
+    public Task<Product?> GetByIdAsync(Guid id)
+    {
+        _products.TryGetValue(id, out var product);
+        return Task.FromResult(product);
+    }
+
+    public Task<Product?> GetByCodeAsync(string code)
+    {
+        var product = _products.Values.FirstOrDefault(p => p.Code == code);
+        return Task.FromResult(product);
+    }
+
+    public Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return Task.FromResult(_products.Values.AsEnumerable());
+    }
+
+    public Task AddAsync(Product product)
+    {
+        _products[product.Id] = product;
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateAsync(Product product)
+    {
+        _products[product.Id] = product;
+        return Task.CompletedTask;
+    }
+}
